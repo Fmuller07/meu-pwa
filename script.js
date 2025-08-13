5ef0b65eb1fab00116558124d9673e7a8c938933
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2025-08-13 10:56:11 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const drawerMenu = document.getElementById('drawer-menu');
+
+    menuToggle.addEventListener('click', () => {
+        drawerMenu.classList.toggle('open');
+    });
+
+    const menuLinks = document.querySelectorAll('.drawer-menu a');
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            drawerMenu.classList.remove('open');
+        });
+    });
+
+    document.body.addEventListener('click', (event) => {
+        if (!drawerMenu.contains(event.target) && !menuToggle.contains(event.target)) {
+            drawerMenu.classList.remove('open');
+        }
+    });
+});
